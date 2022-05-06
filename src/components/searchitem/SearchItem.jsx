50b5d7d2b4093a9b0eea4b8dc292d40775d067cd
prahladtimalsina <prahladtimalsina7@gmail.com>
2022-05-07 00:25:07 +0545

    search list item done

---
+++ src/components/searchitem/SearchItem.jsx
@@ -0,0 +1,41 @@
+import "./searchitem.css";
+
+const SearchItem = () => {
+  return (
+    <div className="searchItem">
+      <img
+        src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/153365577.jpg?k=8f2c50b72adcf6a215d30e2295a6ce770a2c5c5492c4c70831482dbad640490d&o=&hp=1"
+        alt=""
+        className="siImg"
+      />
+      <div className="siDesc">
+        <h1 className="siTitle">Tower Street Apartments</h1>
+        <span className="siDistance">500m from center</span>
+        <span className="siTaxiOp">Free airport taxi</span>
+        <span className="siSubtitle">
+          Studio Apartment with Air conditioning
+        </span>
+        <span className="siFeatures">
+          Entire studio . 1 bathroom .21m^2 1 full bed
+        </span>
+        <span className="siCancelOp">Free cancellation</span>
+        <span className="siCancelOpSubtitle">
+          You can cancel later, so lock in this great price today
+        </span>
+      </div>
+      <div className="siDetails">
+        <div className="siRating">
+          <span>Excellent</span>
+          <button>8.9</button>
+        </div>
+        <div className="siDetailTexts">
+          <span className="siPrice">$123</span>
+          <span className="siTaxOp">Includes taxes and fees</span>
+          <button className="siCheckButton">See availability</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchItem;
