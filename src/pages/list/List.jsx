f37fc692c70fb6aa844af222c22868cb5700b9e7
prahladtimalsina <prahladtimalsina7@gmail.com>
2022-05-06 23:28:37 +0545

    search list pre state fetch location

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -41,6 +41,67 @@ const List = () => {
                 />
               )}
             </div>
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOptions">
+                <div className="lsOptionItem">
+                  <span className="lsOptionsText">
+                    Min price <small>per night</small>
+                  </span>
+                  <input
+                    type="number"
+                    className="lsOptionInput"
+                    name=""
+                    id=""
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionsText">
+                    Max price <small>per night</small>
+                  </span>
+                  <input
+                    type="number"
+                    className="lsOptionInput"
+                    name=""
+                    id=""
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionsText">Adult</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={options.adult}
+                    name=""
+                    id=""
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionsText">Children</span>
+                  <input
+                    type="number"
+                    min={0}
+                    className="lsOptionInput"
+                    placeholder={options.children}
+                    name=""
+                    id=""
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionsText">Room</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={options.room}
+                    name=""
+                    id=""
+                  />
+                </div>
+              </div>
+            </div>
+            <button>Search</button>
           </div>
           <div className="listResult"></div>
         </div>
